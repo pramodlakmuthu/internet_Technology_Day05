dcacadac4e12d45efbdd7b27862958e3f6ccb15f
pramod lakmuthu <pramodlakmuthu09@gmail.com>
2026-07-13 22:47:19 +0530

    Refactor app.js and index.html to comment out old code and add basic arithmetic functionality

--- app.js
+++ app.js
@@ -1,56 +1,56 @@
-let x=5;
-let y="5";
-console.log(x==y);
-let numbers=[1,2,3,4,5];
-for(let i=0;i<numbers.length;i++){
-   //  console.log(numbers[i]);
-}
+// let x=5;
+// let y="5";
+// console.log(x==y);
+// let numbers=[1,2,3,4,5];
+// for(let i=0;i<numbers.length;i++){
+//    //  console.log(numbers[i]);
+// }
 
-class Customer{
+// class Customer{
 
-    name;
-    age;
-    salary;
+//     name;
+//     age;
+//     salary;
 
-    constructor(name,age, salary){
-        this.name=name;
-        this.age=age;
-        this.salary=salary;
-    }
+//     constructor(name,age, salary){
+//         this.name=name;
+//         this.age=age;
+//         this.salary=salary;
+//     }
 
-}
-let customer1=new Customer("pramod",23,55451);
-//
-//console.log(customer1);
+// }
+// let customer1=new Customer("pramod",23,55451);
+// //
+// //console.log(customer1);
 
-let customer2={
-    name: "pramodlakmuthu",
-     age: 24,
-    address: "NO:338/B imbulahena",
-    uni: "GWU",
-    items:[
-        {
-            id:1,
-            name:"variant1",
-            price:10
-        },
-        {
-           id:2,
-            name:"variant2",
-            price:20 
-        },
-         {
-           id:3,
-            name:"variant3",
-            price:30 
-        }
-    ],
-    father:{
-        name:"ananda",
-        age:65,
-        address:"abc street"
-    }
-}
+// let customer2={
+//     name: "pramodlakmuthu",
+//      age: 24,
+//     address: "NO:338/B imbulahena",
+//     uni: "GWU",
+//     items:[
+//         {
+//             id:1,
+//             name:"variant1",
+//             price:10
+//         },
+//         {
+//            id:2,
+//             name:"variant2",
+//             price:20 
+//         },
+//          {
+//            id:3,
+//             name:"variant3",
+//             price:30 
+//         }
+//     ],
+//     father:{
+//         name:"ananda",
+//         age:65,
+//         address:"abc street"
+//     }
+// }
 // console.log(customer2.uni);
 // console.log(customer2.items);
 // console.log(customer2.items[1]);
@@ -72,13 +72,37 @@ let customer2={
 //     myname.innerText="johne mice"+number++;
     
 // }
-let count=document.getElementById("counterId");
-let number=0;
-function countIncreament(){
-    count.innerText="counter: "+number++;
+// let count=document.getElementById("counterId");
+// let number=0;
+// function countIncreament(){
+//     count.innerText="counter: "+number++;
 
+// }
+// function countDecrement(){
+//     count.innerText="counter: "+number--;
+
+// }
+
+// function changeHeading(){
+//     let txtvlaue=document.getElementById("headingInput");
+//     console.log(txtvlaue.value);
+//     let heading=document.getElementById("headdingtxt");
+//     heading.innerText=txtvlaue.value;
+// }
+function add(){
+    let num1=document.getElementById("num1").value;
+    let num2=document.getElementById("num2").value;
+    let result=Number(num1)+Number(num2);
+    console.log(result);
+    let output=document.getElementById("result");
+    output.innerText="Output: "+result;
+    
 }
-function countDecrement(){
-    count.innerText="counter: "+number--;
-
+function sub(){
+    let num1=document.getElementById("num1").value;
+    let num2=document.getElementById("num2").value;
+    let result=Number(num1)-Number(num2) ;
+    console.log(result);
+    let output=document.getElementById("result");
+    output.innerText="Output: "+result;
 }
