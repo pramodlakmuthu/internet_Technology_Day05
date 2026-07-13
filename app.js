d0c866bb7c3ad0c5c200529c7b27d7cc61022c1b
pramod lakmuthu <pramodlakmuthu09@gmail.com>
2026-07-13 21:19:34 +0530

    Refactor app.js to implement Customer class and enhance counter functionality in index.html

--- app.js
+++ app.js
@@ -3,15 +3,82 @@ let y="5";
 console.log(x==y);
 let numbers=[1,2,3,4,5];
 for(let i=0;i<numbers.length;i++){
-    console.log(numbers[i]);
+   //  console.log(numbers[i]);
 }
 
-let customer={
+class Customer{
 
-    name:"pramod",
-    isAfterAL: true,
-    age:23,
-    salary:10000,
+    name;
+    age;
+    salary;
+
+    constructor(name,age, salary){
+        this.name=name;
+        this.age=age;
+        this.salary=salary;
+    }
 
 }
-console.log(customer);
+let customer1=new Customer("pramod",23,55451);
+//
+//console.log(customer1);
+
+let customer2={
+    name: "pramodlakmuthu",
+     age: 24,
+    address: "NO:338/B imbulahena",
+    uni: "GWU",
+    items:[
+        {
+            id:1,
+            name:"variant1",
+            price:10
+        },
+        {
+           id:2,
+            name:"variant2",
+            price:20 
+        },
+         {
+           id:3,
+            name:"variant3",
+            price:30 
+        }
+    ],
+    father:{
+        name:"ananda",
+        age:65,
+        address:"abc street"
+    }
+}
+// console.log(customer2.uni);
+// console.log(customer2.items);
+// console.log(customer2.items[1]);
+// console.log(customer2.father.name,);
+// console.log(customer2.items[1].price);
+
+
+
+//*********************************************************************************************************
+
+// console.log(document.title);
+// console.log(document.activeElement)
+// let heading=document.getElementById("myname");
+// // myname.innerText="U.P.L. Perera";
+// // console.log(heading);
+// let number=0;
+
+// function changeHeading(){
+//     myname.innerText="johne mice"+number++;
+    
+// }
+let count=document.getElementById("counterId");
+let number=0;
+function countIncreament(){
+    count.innerText="counter: "+number++;
+
+}
+function countDecrement(){
+    count.innerText="counter: "+number--;
+
+}
